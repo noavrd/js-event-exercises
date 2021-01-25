873e4ef5fb41500af452fbde36aece3c5ad307e6
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-25 22:32:30 +0200

    finish task 7

---
+++ default-browser-action/Catch-links-in-the-element/script.js
@@ -0,0 +1,14 @@
+let contents = document.getElementById("contents");
+let link = document.getElementsByTagName("href");
+
+contents.onclick = function(event) {
+
+    function getToLink(link){
+        let leave = confirm("Do you really want to leave this site?")
+        if(!leave) return false;
+    }
+    let target = event.target.closest("a");
+    if(contents.contains(target) && target) {
+        return getToLink(target.getAttribute(link))
+    }
+}

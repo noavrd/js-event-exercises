7176f57f13e62e619550bf6ec4dd8ee9ff01158b
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-26 21:23:53 +0200

    finish task 4

--- introduction-browser-events-folder/Move-the ball-across-the-field/script.js
+++ introduction-browser-events-folder/Move-the ball-across-the-field/script.js
@@ -3,6 +3,28 @@ let field = document.getElementById("field");
 
 
 field.onclick = function (event) {
-    
-    
+    let coordsField = this.getBoundingClientRect();
+    let coordsBall = {
+        left: event.clientX - coordsField.left - field.clientLeft - ball.clientWidth / 2,
+        top: event.clientY - coordsField.top - field.clientTop - ball.clientHeight /2
+    };
+
+    if (coordsBall.left < 0) {
+        coordsBall.left = 0;
+    }
+
+    if (coordsBall.top < 0) {
+        coordsBall.top = 0;
+    }
+
+    if ( (coordsBall.left + ball.clientWidth) > field.clientWidth ) {
+        coordsBall.left = field.clientWidth - ball.clientWidth;
+    }
+
+    if ( (coordsBall.top + ball.clientHeight) > field.clientHeight) {
+        coordsBall.top = field.clientHeight - ball.clientHeight;
+    }
+
+    ball.style.left = coordsBall.left + "px";
+    ball.style.top = coordsBall.top + "px";
 }

e27f8239f50fadcb87044119698da83f9f524639
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-26 21:58:18 +0200

    finish task 10

--- event-delegation/Tree-menu/script.js
+++ event-delegation/Tree-menu/script.js
@@ -2,17 +2,17 @@ let tree = document.getElementById("tree");
 let allLi = document.querySelectorAll("li");
 for (let li of allLi) {
     let span = document.createElement("span");
-    li.append(span);
+    li.prepend(span);
     span.appendChild(span.nextSibling);
 }
 
 tree.addEventListener("click", (event) => {
-        if (event.target.tagName !== "SPAN") {
-            return;
-        }
-        let select = event.target.parentNode.querySelector("ul");
-        if (!select) {
-            return;
-        }
-        select.hidden = !select.hidden;
-    });
+    if (event.target.tagName !== "SPAN") {
+        return;
+    }
+    let select = event.target.parentNode.querySelector("ul");
+    if (!select) {
+        return;
+    }
+    select.hidden = !select.hidden;
+});

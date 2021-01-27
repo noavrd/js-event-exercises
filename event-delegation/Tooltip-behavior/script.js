d22fd49a43d8691345a6ef980676856eec0ab55a
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-27 17:34:42 +0200

    finish task 11

--- event-delegation/Tooltip-behavior/script.js
+++ event-delegation/Tooltip-behavior/script.js
@@ -1,34 +1,30 @@
-let button = document.getElementsByTagName("button"); 
-
-document.addEventListener("onmouseover", function(event) {
+let tooltip = document.getElementsByClassName("tooltip"); 
+let elem;
+document.onmouseover = (event) => {
     let tool = event.target.dataset.tooltip;
-    if(!tool) {
+    if (!tool) {
         return;
     }
-
-    let elem = document.createElement("div");
+    elem = document.createElement("div");
     elem.className = "tooltip";
     elem.innerHTML = tool;
     document.body.append(elem);
 
-    let above = event.target.getBoundingClientReact();
-    let left = above.left + (event.target.offsetWidth - elem.offsetWidth) / 2;
-
-    if (left < 0) {
-        left = 0;
-    }
-
-    let top = above.top - elem.offsetHeight - 5;
+    let border = event.target.getBoundingClientRect();
+    let left = border.left;
+    let top = border.top - elem.offsetHeight - 5;
     if (top < 0) {
-        top = above.top + event.target.offsetHeight + 5;
+        top = border.top + event.target.offsetHeight + 5;
     }
+
     elem.style.left = left + "px";
     elem.style.top = top + "px";
-});
 
-document.addEventListener("onmouseout", function() {
-    if (target.dataset.tooltip) {
-        target.dataset.tooltip.remove();
-        target.dataset.tooltip = null;
+};
+
+document.onmouseout = () => {
+    if (elem) {
+        elem.remove();
+        elem = null;
     }
-})
+};
